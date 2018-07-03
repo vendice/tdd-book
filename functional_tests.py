42f0e9cd3395672fb9e566a0b026a9297bba1221
Sebastian Kluge <sebastian.kluge@posteo.de>
2018-07-03 10:52:02 +0200

    testklasse eingefuehrt und testszenario kommentiert

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,39 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest (unittest.TestCase):
 
-assert 'Django' in browser.title,"Django not in browser title!"
+    def setUp(self):
+        self.browser =  webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+
+        # der user hat von einer neuen to-do app geh;rt und besucht sie
+        self.browser.get('http://localhost:8000')
+
+        # er schaut sich den Titel der Seite an, und sieht To-Do                                                 
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!"')
+
+        # Er kann ein to-do item eingeben
+
+        # Er tippt einkaufen ein
+
+        # Als er enter ein gibt wird die seite geupdated und er kann den ersten Punkt einkaufen sehen
+
+        # Die Textbox steht noch immer da, er kann einen zweiten Punkt eingeben
+
+        # nach dem druecken von enter stehen beide Punkte unterhalb der Textbox
+
+        # Die Liste ist ueber eine einyigartige url aufrufbar
+
+        # nach dem aufrufen der url ist der Text noch da
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
 
