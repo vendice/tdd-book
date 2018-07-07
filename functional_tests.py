08095f2124d92687b7ea880cc151f11ee777aa4c
Sebastian Kluge <sebastian.kluge@posteo.de>
2018-07-07 20:48:17 +0200

    Model for list Items and associated migration

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest (unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+    
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
@@ -35,8 +40,8 @@ class NewVisitorTest (unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows =  table.find_elements_by_tag_name('tr')
-        self.assertIn('1: einkaufen' , [row.text for row in rows])
-        self.assertIn('2: essen' , [row.text for row in rows])
+        self.check_for_row_in_list_table('1: einkaufen')
+        self.check_for_row_in_list_table('2: essen')
 
         
         # Die Textbox steht noch immer da, er kann einen zweiten Punkt eingeben
