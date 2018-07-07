99e01ffeb5e6e5804a0a8b85b2c65334b3361bdf
Sebastian Kluge <sebastian.kluge@posteo.de>
2018-07-07 23:18:58 +0200

    middle of chapter

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from lists.models import Item
 
 class HomePageTest(TestCase):
 
@@ -7,13 +8,37 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'],'/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
 
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from lists.models import Item 
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', ''),})
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ''
+
+    return render(request, 'home.html', {'new_item_text': new_item_text})
 
 
 
